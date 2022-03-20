6c79f9d89c14e269dfe5d3e0d79d5540b7df4fed
Nicouicich <nico_uicich@hotmail.com>
2022-03-20 16:00:11 -0300

    change formatter

--- index.js
+++ index.js
@@ -1,83 +1,73 @@
-const fs=require('fs/promises')
-const path=require('path')
+const fs = require('fs/promises')
+const path = require('path')
 
 class Container {
   fileName
 
-
   constructor(fileName) {
-    this.fileName=fileName
+    this.fileName = fileName
   }
 
-  async read () {  // Con esta funcion, al recibir un id < 0 se devuelven todo completo. Si tiene un ID, devuelve unicamente ese objeto ---
+  async read() {
+    // Con esta funcion, al recibir un id < 0 se devuelven todo completo. Si tiene un ID, devuelve unicamente ese objeto ---
     try {
-      const data=await fs.readFile(this.fileName, 'utf-8')
-      return (JSON.parse(data))
-    }
-    catch (err) {
-      console.log("Archivo vacio")
+      const data = await fs.readFile(this.fileName, 'utf-8')
+      return JSON.parse(data)
+    } catch (err) {
+      console.log('Archivo vacio')
     }
   }
 
-  async write (product) {
+  async write(product) {
     try {
-      let id=1
-      let arr=[]
-      let data=await this.read()
+      let id = 1
+      let arr = []
+      let data = await this.read()
 
-      if (product==0) {
-        await fs.writeFile(this.fileName, "")
-        return ("Todos los productos eliminados con exito")
-      }
-      else {
-        product.id=id
-        if (data===undefined)
-          arr=[product]
+      if (product == 0) {
+        await fs.writeFile(this.fileName, '')
+        return 'Todos los productos eliminados con exito'
+      } else {
+        product.id = id
+        if (data === undefined) arr = [product]
         else {
           for (let item of data) {
             arr.push(item)
-            if (id<item.id)
-              id=item.id
+            if (id < item.id) id = item.id
           }
-          product.id=id+1
+          product.id = id + 1
           arr.push(product)
         }
-        arr=JSON.stringify(arr, null, '\t')
+        arr = JSON.stringify(arr, null, '\t')
         await fs.writeFile(this.fileName, arr)
-        return (`El producto se guardo correctamente producto se guardo correctamente con el ID: ${product.id}`)
-
+        return `El producto se guardo correctamente producto se guardo correctamente con el ID: ${product.id}`
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err)
     }
-
   }
 
-  async deleteID (id) {
+  async deleteID(id) {
     try {
-      let arr=[]
-      let deletedID=false
-      let data=await this.read()
-      for (let item of data) { // si le pasan un id para borrar directamente no lo escribo en el nuevo array y seteo deletedID en true
-        if (item.id!=id)
-          arr.push(item)
-        else
-          deletedID=true
+      let arr = []
+      let deletedID = false
+      let data = await this.read()
+      for (let item of data) {
+        // si le pasan un id para borrar directamente no lo escribo en el nuevo array y seteo deletedID en true
+        if (item.id != id) arr.push(item)
+        else deletedID = true
       }
-      arr=JSON.stringify(arr, null, '\t')
+      arr = JSON.stringify(arr, null, '\t')
       await fs.writeFile(this.fileName, arr)
       if (deletedID)
-        return (`El producto con el ID ${id} fue eliminado con exito`)
-      else
-        return ("Producto inexistente")
-    }
-    catch (err) {
+        return `El producto con el ID ${id} fue eliminado con exito`
+      else return 'Producto inexistente'
+    } catch (err) {
       console.log(err)
     }
   }
 
-  save=function (product) {
+  save = function (product) {
     this.write(product)
       .then((resp) => {
         console.log(resp)
@@ -85,42 +75,35 @@ class Container {
       .catch((err) => {
         console.log(err)
       })
-
-
   }
 
-  getById (id) {
+  getById(id) {
     this.read()
       .then((resp) => {
         let idFound
 
-        if (id>0) {
-          const product=resp.find(element => element.id===id)
-          if (product)
-            console.log(product)
-          else
-            throw new Error(null)
-        }
-        else
-          console.log("ID incorrecto")
-
+        if (id > 0) {
+          const product = resp.find((element) => element.id === id)
+          if (product) console.log(product)
+          else throw new Error(null)
+        } else console.log('ID incorrecto')
       })
       .catch((err) => {
         console.log(err)
       })
   }
 
-  getAll () {
+  getAll() {
     this.read()
       .then((resp) => {
         console.log(resp)
       })
       .catch((err) => {
-        console.log("Usuario no encontrado")
+        console.log('Usuario no encontrado')
       })
   }
 
-  deleteById (id) {
+  deleteById(id) {
     this.deleteID(id)
       .then((resp) => {
         console.log(resp)
@@ -130,7 +113,7 @@ class Container {
       })
   }
 
-  deleteAll () {
+  deleteAll() {
     this.write(0)
       .then((resp) => {
         console.log(resp)
@@ -141,12 +124,12 @@ class Container {
   }
 }
 
-container=new Container('./productos.txt')
+container = new Container('./productos.txt')
 
-const obj={
-  "title": "Hoja",
-  "price": 45,
-  "thumbnail": "https"
+const obj = {
+  title: 'Hoja',
+  price: 45,
+  thumbnail: 'https',
 }
 container.save(obj)
 // container.getById(4)
