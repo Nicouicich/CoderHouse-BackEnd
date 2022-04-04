97d6d95146ee65674edd410ac92cc289f94aa033
Nicouicich <nico_uicich@hotmail.com>
2022-04-03 22:31:13 -0300

    features

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const express = require('express')
 
 class Container {
-
   constructor(fileName) {
     this.fileName = fileName
   }
@@ -143,18 +142,71 @@ async function showIDs() {
 }
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 const port = 8080
 const server = app.listen(port, () => {})
 
+let products = [
+  {
+    title: 'Escuadra',
+    price: 1111.45,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+    id: 1,
+  },
+  {
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+    id: 2,
+  },
+  {
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+    id: 3,
+  },
+  {
+    title: 'Hoja',
+    price: 45,
+    thumbnail: 'https',
+    id: 4,
+  },
+]
+
 app.get('/productos', (req, res) => {
-  container
-    .read()
-    .then((resp) => {
-      let arr = []
-      resp.map((prod) => arr.push(prod))
-      res.send(arr)
-    })
-    .catch((err) => res.send(err))
+  // container
+  //   .read()
+  //   .then((resp) => {
+  //     let arr = []
+  //     resp.map((prod) => arr.push(prod))
+  //     res.send(arr)
+  //   })
+  //   .catch((err) => res.send(err))
+  res.status(201).json({
+    data: products,
+  })
+})
+
+app.get('/productos/:id', (req, res) => {
+  const idBuscado = req.params.id
+  const id = products.findIndex((prod) => prod.id == idBuscado)
+  // container
+  //   .read()
+  //   .then((resp) => {
+  //     res.send(resp.find((prod) => prod.id == idBuscado))
+  //   })
+  //   .catch((err) => res.send(err))
+  if (id != -1)
+    res.status(201).json({
+      data: products[id],
+  })
+  res.status(404).json({
+    msg: 'Producto inexistente',
+  })
 })
 
 app.get('/productoRandom', (req, res) => {
@@ -164,10 +216,90 @@ app.get('/productoRandom', (req, res) => {
       resp.forEach((element) => {
         ids.push(element)
       })
-      let id = ids [Math.floor(Math.random() * ids.length)]
+      let id = ids[Math.floor(Math.random() * ids.length)]
       getById(id)
         .then((resp) => res.send(resp))
         .catch((err) => console.log(res))
     })
     .catch((err) => console.log(err))
+})
+
+app.put('/productos/:id', (req, res) => {
+  const searchId = Number(req.params.id)
+  const body = req.body
+  const pos = products.findIndex((prod) => prod.id == searchId)
+
+  if (pos==-1) {
+    return res.status(404).json({
+      msg: { error: 'Producto inexistente' },
+    })
+  }
+  if (
+    !body.title ||
+    !body.price ||
+    !body.thumbnail||
+    typeof body.title != 'string' ||
+    typeof body.price != 'number' ||
+    typeof body.thumbnail != 'string'
+  )
+    return res.status(400).json({
+      msg: 'Producto ingresado de manera incorrecta',
+    })
+
+  products[pos].title = body.title
+  products[pos].price = body.price
+  products[pos].thumbnail = body.thumbnail
+  res.status(201).json({
+    data: products[pos],
+  })
+})
+
+app.post('/productos/', (req, res) => {
+  const searchId = Number(req.params.id)
+  const body = req.body
+
+
+
+
+  if (
+    !body.title ||
+    !body.price ||
+    !body.thumbnail||
+    typeof body.title != 'string' ||
+    typeof body.price != 'number' ||
+    typeof body.thumbnail != 'string'
+  )
+    return res.status(400).json({
+      msg: 'Producto ingresado de manera incorrecta',
+    })
+    const newProduct = {
+      id :products.length+1,
+      title: body.title,
+      price: body.price,
+      thumbnail: body.thumbnail
+  
+    }
+
+  products.push(newProduct)
+
+  res.status(201).json({
+    data: newProduct,
+  })
+})
+
+
+app.delete('/productos/:id', (req, res) => {
+  const searchId = Number(req.params.id)
+  const pos = products.findIndex((prod) => prod.id == searchId)
+
+  if (pos==-1) {
+    return res.status(404).json({
+      msg: { error: 'Producto inexistente' },
+    })
+  }
+  const arr = products.splice(pos,1)
+ 
+  res.status(201).json({
+    data: products,
+  })
 })
